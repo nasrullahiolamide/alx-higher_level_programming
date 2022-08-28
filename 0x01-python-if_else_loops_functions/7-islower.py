33a05d3fbd50b38e896f81441a9a1bc4ab894574
nasrullahiolamide <nasrullahiolamide3011@gmail.com>
2022-08-28 22:58:56 +0100

    is lower

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-    if c in range(97, 123):
-        print("{} is lower".format(chr(c)))
-    elif c in range(65, 91):
-        print("{} is upper".format(chr(c)))
+    if c in range(97, 123) or ord(c) in range(97, 123):
+        return True
+    else:
+        return False
