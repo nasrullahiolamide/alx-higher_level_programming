05b9ec6a5389b87eaf57dbf15e99b2c377d1287d
nasrullahiolamide <nasrullahiolamide3011@gmail.com>
2022-08-29 12:39:40 +0100

    uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in str:
+        print("{}"
+              .format(chr(ord(i)-32) if
+                      (ord(i) >= 97 and ord(i) <= 122) else i), end="")
+        if (i == str[-1]):
+            print("{}".format('\n'))
