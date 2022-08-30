d95dab6525e4ed115b775f60179ffcca7d94ce12
nasrullahiolamide <nasrullahiolamide3011@gmail.com>
2022-08-30 13:04:35 +0100

    replace and retrieve item

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
+    length = len(my_list)
     if idx < 0:
         return None
-    elif idx > len(my_list):
+    elif idx > length - 1:
         return None
     elif idx >= 0 and idx <= len(my_list):
         return my_list[idx]
