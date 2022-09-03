55bfa171004158b94c932565287ad3f23f7bf0fd
nasrullahiolamide <nasrullahiolamide3011@gmail.com>
2022-09-04 00:29:07 +0100

    argv element

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,7 +7,10 @@ if __name__ == "__main__":
     if (len(arg_items) - 1 == 0):
         print("{:d} arguments.".format(0))
     else:
-        print("{:d} arguments:".format(len(arg_items) - 1))
+        if (len(arg_items) - 1 == 1):
+            print("{:d} argument:".format(len(arg_items) - 1))
+        else:
+            print("{:d} arguments:".format(len(arg_items) - 1))
         i = 1
         while i < len(arg_items):
             print("{}: {}".format(i, arg_items[i]))
