bf6351789fa0e887fdd6b9c458cfaaa44625a513
nasrullahiolamide <nasrullahiolamide3011@gmail.com>
2022-09-04 11:05:05 +0100

    import fix

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,10 +2,10 @@
 import hidden_4
 
 if __name__ == "__main__":
-        names = dir(hidden_4)
+    names = dir(hidden_4)
 
-        i = 1
-        while i <= (len(names) - 1):
-                if names[i][0:2] != "__":
-                        print(names[i])
-                i = i + 1
+    i = 1
+    while i <= (len(names) - 1):
+        if names[i][0:2] != "__":
+            print(names[i])
+        i = i + 1
