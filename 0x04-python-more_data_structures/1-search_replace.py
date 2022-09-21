9d38fe0ba63078f98e686d0b201c91d8aac1e4ab
nasrullahiolamide <nasrullahiolamide3011@gmail.com>
2022-09-21 15:24:12 +0100

    find and replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -3,8 +3,10 @@
 def search_replace(my_list, search, replace):
     new_list = my_list.copy()
     idx = 0
-    while new_list[idx] <= len(new_list):
-        if new_list[idx] == search:
+    while idx < len(new_list):
+        if idx > len(new_list):
+            raise IndexError
+        elif new_list[idx] == search:
             new_list[idx] = replace
         idx = idx + 1
     return new_list
