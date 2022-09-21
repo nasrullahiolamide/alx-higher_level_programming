611095aabaf1a62afe787d6c0084c61dafa8e873
nasrullahiolamide <nasrullahiolamide3011@gmail.com>
2022-09-21 13:02:27 +0100

    find and replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -3,7 +3,7 @@
 def search_replace(my_list, search, replace):
     new_list = my_list.copy()
     idx = 0
-    while new_list[idx] < len(new_list):
+    while new_list[idx] <= len(new_list):
         if new_list[idx] == search:
             new_list[idx] = replace
         idx = idx + 1
