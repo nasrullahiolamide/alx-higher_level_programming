26ad28f15f98291f2a071b5844b610eedf2959b5
nasrullahiolamide <nasrullahiolamide3011@gmail.com>
2022-09-22 16:52:54 +0100

    multiply value by 2

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def multiply_by_2(a_dictionary):
+    new_dict = a_dictionary.copy()
+    list_of_key = list(new_dict.keys())
+    for i in list_of_key:
+        new_dict[i] = new_dict[i] * 2
+    return new_dict
