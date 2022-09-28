db25f887bd48e2d55273a6429ea307361e4c2b09
nasrullahiolamide <nasrullahiolamide3011@gmail.com>
2022-09-28 15:00:51 +0100

    read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+'''
+Read file
+'''
 def read_file(filename=""):
+    '''
+    read_file function
+    '''
     with open(filename, encoding="utf-8") as f:
         print(f.read())
