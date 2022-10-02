1436f230fb25e090ce2b6bdc4fbc74d9afd41ba2
nasrullahiolamide <nasrullahiolamide3011@gmail.com>
2022-10-02 11:50:35 +0100

    update square

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""
+No module imported
+"""
+
+
+class Square:
+    """
+        A class that defines as Square
+
+        -----------
+        Private instance attribute: size
+        Instantiation with optional size: def __init__(self, size=0):
+        Public instance method: def area(self): that returns the current square area
+    """
+
+    def __init__(self, size=0):
+        """
+        Instantiation with optional size: def __init__(self, size=0)
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """
+        property def size(self) to get the size attribute
+        """
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """
+        property setter def size(self, value): to update the value of the size
+        """
+        self.__size = value
+        try:
+            assert isinstance(value, int)
+        except BaseException:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """
+        public instance method
+        returns the current square area
+        """
+        return (self.__size ** 2)
