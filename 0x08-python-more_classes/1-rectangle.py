3749ec71943146d980eae11b86912b650576a094
nasrullahiolamide <nasrullahiolamide3011@gmail.com>
2022-10-03 15:44:43 +0100

    width and height setter and getter

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -15,8 +15,8 @@ class Rectangle:
         Initializing function with private attribute
         __width and __height
         """
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
