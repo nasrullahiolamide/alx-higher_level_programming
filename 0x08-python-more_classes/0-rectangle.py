de4bb9d5569f37455b44a868ca924ae0ac2199e9
nasrullahiolamide <nasrullahiolamide3011@gmail.com>
2022-10-03 15:02:36 +0100

    Empty class

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+No imported Module
+Module will contain class definition of a rectangle
+"""
+
+
+class Rectangle:
+    """
+    empty defintion with a pass statement
+    """
+    pass
