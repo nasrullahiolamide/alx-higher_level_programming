ff638965432a8f3e57368556e7a717f3a50dc686
nasrullahiolamide <nasrullahiolamide3011@gmail.com>
2022-10-03 15:46:02 +0100

    width and height setter and getter

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -20,7 +20,7 @@ class Rectangle:
 
     @property
     def width(self):
-        return self.width
+        return self.__width
 
     @width.setter
     def width(self, value):
@@ -35,7 +35,7 @@ class Rectangle:
 
     @property
     def height(self):
-        return self.height
+        return self.__height
 
     @height.setter
     def height(self, value):
