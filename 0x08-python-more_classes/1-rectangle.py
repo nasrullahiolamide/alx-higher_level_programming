7326a9dd57fe138059a45bc8d330e241725adca6
nasrullahiolamide <nasrullahiolamide3011@gmail.com>
2022-10-03 15:42:55 +0100

    width and height setter and getter

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -30,6 +30,8 @@ class Rectangle:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
+        self.__width = value
+
 
     @property
     def height(self):
@@ -43,3 +45,4 @@ class Rectangle:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
+        self.__height = value
