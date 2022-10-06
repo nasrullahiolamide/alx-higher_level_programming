6eb6645194e3ede5e02564b3308361912fc7710d
nasrullahiolamide <nasrullahiolamide3011@gmail.com>
2022-10-06 18:31:27 +0100

    final

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,44 +1,19 @@
-# #!/usr/bin/python3
-# """
-# No module imported
-# """
-
-
-# def add_integer(a, b=98):
-#     """
-#     A funtion that adds two integers and return their sum
-#     """
-#     try:
-#         assert isinstance(a, (int, float))
-#     except BaseException:
-#         raise TypeError("a must be an integer")
-#     try:
-#         assert isinstance(b, (int, float))
-#     except BaseException:
-#         raise TypeError("b must be an integer")
-#     if type(a) == float:
-#         a = int(a)
-#     if type(b) == float:
-#         b = int(b)
-#     return a + b
-
-
 #!/usr/bin/python3
 """
-No module importation allowed for this task
+No module imported
 """
 
 
 def add_integer(a, b=98):
     """
-        Adds two integers
+    A funtion that adds two integers and return their sum
     """
     try:
-        assert type(a) in (float, int)
+        assert isinstance(a, (int, float))
     except BaseException:
         raise TypeError("a must be an integer")
     try:
-        assert type(b) in (float, int)
+        assert isinstance(b, (int, float))
     except BaseException:
         raise TypeError("b must be an integer")
     if isinstance(a, float):
