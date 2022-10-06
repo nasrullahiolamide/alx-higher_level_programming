3810e0d77da4098dfe4c65039666e5affd1b8ee0
nasrullahiolamide <nasrullahiolamide3011@gmail.com>
2022-10-06 18:20:30 +0100

    add integer

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,23 +1,48 @@
+# #!/usr/bin/python3
+# """
+# No module imported
+# """
+
+
+# def add_integer(a, b=98):
+#     """
+#     A funtion that adds two integers and return their sum
+#     """
+#     try:
+#         assert isinstance(a, (int, float))
+#     except BaseException:
+#         raise TypeError("a must be an integer")
+#     try:
+#         assert isinstance(b, (int, float))
+#     except BaseException:
+#         raise TypeError("b must be an integer")
+#     if type(a) == float:
+#         a = int(a)
+#     if type(b) == float:
+#         b = int(b)
+#     return a + b
+
+
 #!/usr/bin/python3
 """
-No module imported
+No module importation allowed for this task
 """
 
 
 def add_integer(a, b=98):
     """
-    A funtion that adds two integers and return their sum
+        Adds two integers
     """
     try:
-        assert isinstance(a, (int, float))
+        assert type(a) in (float, int)
     except BaseException:
         raise TypeError("a must be an integer")
     try:
-        assert isinstance(b, (int, float))
+        assert type(b) in (float, int)
     except BaseException:
         raise TypeError("b must be an integer")
-    if type(a) == float:
+    if isinstance(a, float):
         a = int(a)
-    if type(b) == float:
+    if isinstance(b, float):
         b = int(b)
     return a + b
