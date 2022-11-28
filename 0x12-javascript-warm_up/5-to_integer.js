6aaec2d1b0fa1b17cb4301647fbce8d9a00b0c19
nasrullahiolamide <nasrullahiolamide3011@gmail.com>
2022-11-28 12:17:38 +0100

    to integer

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const process = require('process');
+
+const parsed = parseInt(process.argv[2], 10);
+
+if (isNaN(parsed)) {
+  console.log('Not a number');
+} else {
+  console.log(`My Number: ${parsed}`);
+}
