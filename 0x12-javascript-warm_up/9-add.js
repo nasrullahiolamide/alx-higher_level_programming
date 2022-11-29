0b7384c308ea794fc1a466007533f4e9749ae3ab
nasrullahiolamide <nasrullahiolamide3011@gmail.com>
2022-11-29 08:27:28 +0100

    add numbers

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const process = require('process');
+
+const a = parseInt(process.argv[2], 10);
+const b = parseInt(process.argv[3], 10);
+
+function add(x, y) {
+  return(x+y)
+}
+
+
+console.log(add(a,b))
