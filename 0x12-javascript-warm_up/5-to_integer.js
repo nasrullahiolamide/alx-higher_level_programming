841571cf12742ee7ac7ab469c6b6e6e4cfcc37ec
nasrullahiolamide <nasrullahiolamide3011@gmail.com>
2022-12-08 18:25:47 +0100

    edited output

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -6,5 +6,5 @@ const parsed = parseInt(process.argv[2], 10);
 if (isNaN(parsed)) {
   console.log('Not a number');
 } else {
-  console.log(`My Number: ${parsed}`);
+  console.log(`My number: ${parsed}`);
 }
