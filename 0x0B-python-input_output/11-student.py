97ff1e8369851445926ab622b2d1e5ebfd265039
nasrullahiolamide <nasrullahiolamide3011@gmail.com>
2022-12-28 17:03:29 +0100

    reload from json

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+""" Class Module """
+
+
+class Student:
+    """ Student model class"""
+
+    def __init__(self, first_name, last_name, age):
+        """ initializing function """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ Serializing function """
+        new_dict = {}
+
+        # covert dict rep into list of tuples
+        for key, value in self.__dict__.items():
+            # chck if attr list is empty to return original dict
+            if (isinstance(attrs, list) and len(attrs) == 0):
+                return new_dict
+            elif (not attrs):
+                return self.__dict__
+            else:
+                # loop over attr to check if it matches the key
+                # to return modified dict
+                for attr in attrs:
+                    if attr in key:
+                        new_dict[attr] = value
+
+        self.__dict__ = new_dict
+
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """
+        load a json from a file and modify and object
+        with its persistent data
+        """
+        for k, v in json.items():
+            setattr(self, k, v)
