0f25e8c272f92339d3ce5f0e90f9ba26c475db61
nasrullahiolamide <nasrullahiolamide3011@gmail.com>
2022-12-28 16:18:24 +0100

    attr preerence

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" Class Module """
+
+
+class Student:
+    """ Student model class"""
+
+    def __init__(self, first_name, last_name, age):
+        """ initializing function """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ Serializing function """
+        new_dict = {}
+
+        # covert dict rep into list of tuples
+        for key, value in self.__dict__.items():
+            # chck if attr list is empty
+            if (not attrs):
+                return self.__dict__
+            else:
+                # loop over attr to check if it matches the key
+                for attr in attrs:
+                    if attr in key:
+                        new_dict[attr] = value
+
+        self.__dict__ = new_dict
+
+        return self.__dict__
