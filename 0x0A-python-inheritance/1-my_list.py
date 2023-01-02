8b81a596611c718a7037bf196c322982807cc72a
nasrullahiolamide <nasrullahiolamide3011@gmail.com>
2023-01-02 18:21:42 +0100

    test written

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -4,7 +4,14 @@ print sorted list
 """
 
 class MyList(list):
+    """"
+    My list class
+    accepts a list as an argument
+    """
 
     def print_sorted(self):
+        """
+        Sort function (method)
+        """
         sorted_list = self.sort()
         print(self)
