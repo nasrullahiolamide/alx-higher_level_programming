5689b4ed5c33be5ed296dba2aee1e22bff9ed915
nasrullahiolamide <nasrullahiolamide3011@gmail.com>
2023-01-02 19:22:39 +0100

    refactor code

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -3,12 +3,11 @@
 To check if it is a subclass
 """
 
-
 def is_same_class(obj, a_class):
     """
     function to check if an obj is a_class inst
     """
-    if (isinstance(obj, a_class)):
+    if (type(obj)== a_class):
         return True
     else:
         return False
