6e8d58cb012dced2271874f96aed9a084a243bfd
nasrullahiolamide <nasrullahiolamide3011@gmail.com>
2023-01-02 19:18:02 +0100

    is same class

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+To check if it is a subclass
+"""
+
+def is_same_class(obj, a_class):
+    """
+    function to check if an obj is a_class inst
+    """
+    if (type(obj)== a_class):
+        return True
+    else:
+        return False
