6fd48dca1c3c3d431dd1afd58caaf0eff33a5667
nasrullahiolamide <nasrullahiolamide3011@gmail.com>
2023-01-03 09:03:47 +0100

    empty class

--- 0x0A-python-inheritance/5-base_geometry.py
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -4,6 +4,9 @@ Base Geometry Module
 """
 
 class BaseGeometry(object):
+    """
+    BaseGeometry Class
+    """
     pass
     # def area(self):
     #     raise Exception("Area is not implemented")
