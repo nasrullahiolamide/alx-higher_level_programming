4c522f458e01821330b2dd18f58d05f451a91d32
nasrullahiolamide <nasrullahiolamide3011@gmail.com>
2023-01-03 08:37:28 +0100

    in kind of

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""
+To check if it is a kind of class
+"""
+
+def is_kind_of_class(obj, a_class):
+    if (isinstance(obj, a_class)):
+        return True
+    else:
+        return False
+      
