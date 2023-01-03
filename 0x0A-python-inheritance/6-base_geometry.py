ec2add84859b4a5b43caa99085ca9b59707bdd12
nasrullahiolamide <nasrullahiolamide3011@gmail.com>
2023-01-03 09:05:59 +0100

    improve geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Base Geometry Module
+"""
+
+
+class BaseGeometry(object):
+    """
+    BaseGeometry Class
+    """
+    
+    def area(self):
+        raise Exception("Area is not implemented")
