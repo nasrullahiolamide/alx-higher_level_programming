d5826e1db86dd87e65eb63281745b487fe834fa8
nasrullahiolamide <nasrullahiolamide3011@gmail.com>
2023-02-24 12:12:12 +0100

    --ammend

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -5,15 +5,15 @@ import MySQLdb
 
 if __name__ == '__main__':
     db = MySQLdb.connect(
-        host = 'localhost',
-        port = 3306,
-        user= argv[1],
-        passwd = argv[2],
-        db = argv[3]
+        host='localhost',
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3]
     )
 
     cursor = db.cursor()
-    
+
     cursor.execute(
         '''
         SELECT cities.id, cities.name, states.name
